14c325300d62a2bfec8c5de8373cb78cf356867c
Raphael Jacob <r.jacob2002@gmail.com>
2018-10-07 20:57:55 +0200

    Add class Server

---
+++ src/python/main/communication/Server.py
@@ -0,0 +1,23 @@
+#! /usr/bin/env python3
+# -*- coding: utf-8 -*-
+#
+
+from twisted.internet.protocol import Factory
+from .ServerConnection import ServerConnection
+
+
+class Server(Factory):
+    def __init__(self, datahandler, config):
+        # datahandler is a method (data, client instance) to handle incoming data
+        # config is a serverconfig dict
+        self.clients = {}
+        self.datahandler = datahandler
+        self.config = config
+
+    def buildProtocol(self, addr):
+        # build connection and return it
+        return ServerConnection(self)
+
+    def handledata(self, data, client):
+        # call the datahandler
+        self.datahandler(data, client)
