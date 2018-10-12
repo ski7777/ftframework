85d84d83dcb3370afcd522b63718f513bc5cca60
Raphael Jacob <r.jacob2002@gmail.com>
2018-10-12 12:13:47 +0200

    Add CallComposer

---
+++ src/python/main/peripherals/CallComposer.py
@@ -0,0 +1,18 @@
+#! /usr/bin/env python3
+# -*- coding: utf-8 -*-
+#
+
+
+def composeCalls(cls):
+    # initialize fresh dict
+    calls = {}
+    # iterate over all parents and the class itself
+    for c in list(cls.__bases__) + [cls]:
+        # check whether attribute is available
+        if hasattr(c, 'calls'):
+            # update calls
+            calls.update(c.calls)
+    # set calls in class
+    cls.calls = calls
+    # return class
+    return(cls)
