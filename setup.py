abc22d89328455aaed7991709141d68c8efe1749
Raphael Jacob <r.jacob2002@gmail.com>
2018-10-25 16:18:48 +0200

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup, Extension
+
+setup(name='ftframework',
+      description='A simple framework for multi-client fischertechnik constructions',
+      version='0.0.1',
+      author='Raphael Jacob',
+      author_email='r.jacob2002@gmail.com',
+      url='https://github.com/ski7777/ftframework',
+      license='GPLv3',
+      packages=['ftframework']
+      )
