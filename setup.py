b4b412518e62cdec18178fee74a9ffc51302c753
Raphael Jacob <r.jacob2002@gmail.com>
2019-02-02 16:31:42 +0100

    Bump Version 1.0.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(name='ftframework',
       description='A simple framework for multi-client fischertechnik constructions',
-      version='0.0.1',
+      version='1.0.0',
       author='Raphael Jacob',
       author_email='r.jacob2002@gmail.com',
       url='https://github.com/ski7777/ftframework',
